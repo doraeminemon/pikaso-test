0e3cf414d1699ae6ac38361da9f05a3093876089
Tyson Do <doraeminemon@gmail.com>
2024-06-05 09:05:15 +0700

    Import Pikaso

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Pikaso, { ShapeModel } from 'pikaso';
+import { onMounted, ref } from 'vue';
+
+const editor = ref(null)
+
+onMounted(() => {
+  const container = window.document.querySelector('#canvas-container')
+  const pikaso = new Pikaso({
+    container,
+    width: 500,
+    height: 500,
+  })
+  editor.value = pikaso
+  // pikaso.board.background.overlay.node.setAttrs(
+  //   {
+  //     fill: "transparent",
+  //     fillLinearGradientColorStops: [0, "red", .5, "green", 1, "blue"],
+  //     fillLinearGradientStartPoint: { x: 0, y: 0 },
+  //     fillLinearGradientEndPoint: { x: 90, y: 50 }
+  //   }
+  // )
+  pikaso.shapes.circle.insert({
+    radius: 100,
+    x: 250,
+    y: 250,
+    fill: 'tomato'
+  })
+  pikaso.board.history.undo = (() => {
+    console.log('undo')
+  })
+})
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="canvas-container" class="canvas-container">
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
-
 <style scoped>
 .logo {
   height: 6em;
@@ -26,5 +51,9 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+#canvas-container {
+  border: solid 1px red;
+  position: relative;
 }
 </style>
